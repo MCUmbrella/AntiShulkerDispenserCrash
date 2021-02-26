e9553f5e1cc2eef67dd946dd09ec39764c01b1dc
MCUmbrella <mcumbrella@protonmail.com>
2021-02-27 01:25:29 +0800

    1.1

--- src/main/java/net/e621/mcumbrella/antishulkerdispensercrash/A.java
+++ src/main/java/net/e621/mcumbrella/antishulkerdispensercrash/A.java
@@ -2,10 +2,6 @@ package net.e621.mcumbrella.antishulkerdispensercrash;
 
 import com.google.inject.Inject;
 import org.slf4j.Logger;
-import org.spongepowered.api.block.tileentity.TileEntity;
-import org.spongepowered.api.block.tileentity.carrier.Dispenser;
-import org.spongepowered.api.block.tileentity.*;
-import org.spongepowered.api.block.tileentity.carrier.ShulkerBox;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.item.ItemType;
@@ -16,15 +12,13 @@ import org.spongepowered.api.event.block.*;
 import org.spongepowered.api.block.*;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.util.Direction;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.api.world.World;
 
 import java.util.HashSet;
 
 @Plugin(
         id = "asdc",
         name = "AntiShulkerDispenserCrash",
-        version = "1.0",
+        version = "1.1",
         url = "https://github.com/MCUmbrella/AntiShulkerDispenserCrash/",
         authors = {
                 "MCUmbrella"
@@ -65,11 +59,8 @@ public class A {
                 (e.getTargetBlock().getPosition().getY()==0&&e.getTargetBlock().getExtendedState().get(Keys.DIRECTION).get()==Direction.DOWN))
         )
         {
-            //e.setCancelled(true);
-            //Dispenser d=(Dispenser)e.getTargetBlock().getLocation().get().getTileEntity().get();
             Inventory i=(Inventory)e.getTargetBlock().getLocation().get().getTileEntity().get();
             for(ItemType b:blkList){if(i.contains(b)){e.setCancelled(true);l.warn("Blocked possible shulker box dispenser crash at: X="+e.getTargetBlock().getPosition().getX()+", Y="+e.getTargetBlock().getPosition().getY()+", Z="+e.getTargetBlock().getPosition().getZ());break;}}
-            //l.warn("Blocked possible shulker box dispenser crash at: X="+e.getTargetBlock().getPosition().getX()+", Y="+e.getTargetBlock().getPosition().getY()+", Z="+e.getTargetBlock().getPosition().getZ());
         }
     }
 }
